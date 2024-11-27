d7d3d871ad6ed2bc3a8c54acc05d6e3defcebedd
ekalka <hamster.nyasha@gmail.com>
2024-11-27 11:42:53 +0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ekalka.github.io',
+    site: 'https://ekalka.github.io/astro-iktdoc',
     base: '/astro-iktdoc',
 })
